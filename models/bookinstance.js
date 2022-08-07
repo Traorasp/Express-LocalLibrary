4a63c3729894c2c9431cbe189ad2ceb75bc7ab6f
Traorasp <miguelsolanopaz@outlook.com>
2022-08-07 11:46:38 -0400

    First version of application moved into GitHub

---
+++ models/bookinstance.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    imprint: {type: String, required: true},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    due_back: {type: Date, default: Date.now}
+  }
+);
+
+// Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+BookInstanceSchema
+.virtual('due_back_dated')
+.get(function() {
+  return formatDate(this.due_back);
+})
+
+function formatDate(date) {
+  var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+
+//Export model
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
